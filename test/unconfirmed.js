4f93ac7380fef0fdb8c45b2f87a0c4de549d8694
刘衍晴 <liu3610yan@hotmail.com>
2018-04-08 21:21:24 +0800

    add associated unit test

---
+++ test/unconfirmed.js
@@ -0,0 +1,52 @@
+/**
+ * @Creator: eyes
+ * @Date: 4/8/18
+ */
+'use strict';
+import test from 'ava';
+import {ExcelModel, Empty, Delete, Insert, Change} from '../src/index';
+
+let state = {
+    '1': {
+        c: {
+            '0:1': {v: 1},
+            '0:2': {v: 1},
+            '1:1': {v: 1},
+            '1:2': {v: 1},
+        }
+    }
+};
+
+test('Excel.apply generate unconfirmed ops for co ops', (t) => {
+    let model = ExcelModel.fromJSON({
+        state
+    });
+    let newModel = model.apply([new Change('1', ['c', 0, 1, 'v'], 2, null), new Change('1', ['c', 1, 1, 'v'], 2, null)]);
+
+    t.is(newModel.unconfirmed.length, 2);
+
+    t.is(
+        JSON.stringify(newModel.state),
+        JSON.stringify({"1":{"c":{"0:1":{"v":2},"0:2":{"v":1},"1:1":{"v":2}, "1:2":{"v":1}}}})
+    );
+
+    t.is(
+        JSON.stringify(newModel.state),
+        JSON.stringify((model.apply(newModel.unconfirmed)).state)
+    );
+});
+
+test('Excel.apply do not generate unconfirmed ops for local ops', (t) => {
+    let model = ExcelModel.fromJSON({
+        state
+    });
+
+    let newModel = model.apply([new Change('1', ['filter'], {colRange:[1,10], row: 0}, null)]);
+    t.is(newModel.unconfirmed.length, 0);
+
+    newModel = model.apply([new Change('1', ['filterByValue'], {2: ['to', 'show']}, null)]);
+    t.is(newModel.unconfirmed.length, 0);
+
+    newModel = model.apply([new Change('1', ['hiddenRows'], [1], null)]);
+    t.is(newModel.unconfirmed.length, 0);
+});
--- test/util.js
+++ test/util.js
@@ -4,7 +4,8 @@
 'use strict';
 import test from "ava";
 
-import {convertCoor, inMergeCell} from '../src/util';
+import {convertCoor, inMergeCell, splitOps, inFilter, trimObj} from '../src/util';
+import {Change} from '../src';
 
 test('convertCoor', (t) => {
     const [row, col] = convertCoor('1:1');
@@ -15,4 +16,38 @@ test('convertCoor', (t) => {
 test('inMergeCell', (t) => {
     let isInMergeCell = inMergeCell({'1:1': {rowspan: 3, colspan: 2}}, [1, 1, 3, 3]);
     t.is(isInMergeCell, true);
+});
+
+test('util-splitOps', t => {
+    let c = new Change('1', ['c', 0, 1, 'v'], 2, null),
+        f = new Change('1', ['filter'], {colRange:[1,10], row: 0}, null),
+        fv = new Change('1', ['filterByValue'], {2: ['to', 'show']}, null),
+        h = new Change('1', ['hiddenRows'], [1], null);
+    let {coOps} = splitOps([c, f, fv, h]);
+    t.is(coOps.length, 1);
+    t.is(coOps[0].p[0], 'c');
+});
+
+test('inFilter', t => {
+    let filter1 = { colRange: [1,3], row: 1 },
+        filter2 = { colRange: [1,3], row: 0 },
+        filter3 = { colRange: [1,3], row: 0 };
+
+    let check1 = inFilter(0, 2, filter1),
+        check2 = inFilter(0, 4, filter2),
+        check3 = inFilter(0, 3, filter3);
+
+    t.is(check1, false);
+    t.is(check2, false);
+    t.is(check3, true);
+});
+
+test('trimObj', t => {
+    let target = {
+        props1: null,
+        props2: true
+    };
+    t.is(Object.keys(target).length, 2);
+    target = trimObj(target);
+    t.is(Object.keys(target).length, 1);
 });
