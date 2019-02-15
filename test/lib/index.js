b931b4533b5c2c56534058f5cde26d5a7ec7ffab
ximing <807754634@qq.com>
2019-02-16 01:09:38 +0800

    fix: change ava config

--- test/lib/index.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by ximing on 12/27/17.
- */
-'use strict';
-
--- webpack.build.lib.config.js
+++ webpack.build.lib.config.js
@@ -1,29 +1,25 @@
 /**
  * Created by yeanzhi on 17/2/25.
  */
-'use strict';
-const {
-    resolve
-} = require('path');
-const webpack = require('webpack');
-var node_modules = resolve(__dirname, 'node_modules');
-var pathToReact = resolve(node_modules, 'react/dist/react.min.js');
+"use strict";
+const { resolve } = require("path");
+const webpack = require("webpack");
+var node_modules = resolve(__dirname, "node_modules");
+var pathToReact = resolve(node_modules, "react/dist/react.min.js");
 
 module.exports = {
     entry: {
-        'dxexcel-state': [
-            './src/index.js'
-        ]
+        "dxexcel-state": ["./src/index.js"]
     },
     output: {
-        filename: '[name].js',
-        sourceMapFilename: '[file].map',
-        path: resolve(__dirname, 'dist'),
-        publicPath: '/dist',
-        library: 'dxexcel-state',
-        libraryTarget: 'umd'
+        filename: "[name].js",
+        sourceMapFilename: "[file].map",
+        path: resolve(__dirname, "dist"),
+        publicPath: "/dist",
+        library: "dxexcel-state",
+        libraryTarget: "umd"
     },
-    devtool: 'cheap-module-source-map',
+    devtool: "cheap-module-source-map",
 
     performance: {
         hints: false
@@ -32,58 +28,49 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                use:[{
-                    loader: 'babel-loader',
-                    options: {
-                        'env': {},
-                        'ignore': [
-                            'node_modules/**',
-                            'dist'
-                        ]
+                use: [
+                    {
+                        loader: "babel-loader"
                     }
-                }],
+                ],
                 exclude: /node_modules/
             },
             {
-                test:/\.css$/,
-                use: ['style-loader','css-loader','postcss-loader']
+                test: /\.css$/,
+                use: ["style-loader", "css-loader", "postcss-loader"]
             },
             {
                 test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            }, {
+                use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+            },
+            {
                 test: /\.(png|jpg|jpeg|gif|woff|svg|eot|ttf|woff2)$/i,
-                use: ['url-loader']
+                use: ["url-loader"]
             }
         ]
     },
     externals: [
         {
             react: {
-                root: 'React',
-                commonjs2: 'react',
-                commonjs: 'react',
-                amd: 'react'
+                root: "React",
+                commonjs2: "react",
+                commonjs: "react",
+                amd: "react"
             },
-            'react-dom':{
-                root: 'ReactDOM',
-                commonjs2: 'react-dom',
-                commonjs: 'react-dom',
-                amd: 'react-dom'
+            "react-dom": {
+                root: "ReactDOM",
+                commonjs2: "react-dom",
+                commonjs: "react-dom",
+                amd: "react-dom"
             }
         }
     ],
     plugins: [
         new webpack.NamedModulesPlugin(),
         new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('production')
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
             }
-        }),
+        })
     ]
 };
